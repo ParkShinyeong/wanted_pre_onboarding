652cc2d4d3313fd37674a483cea9229f64b48330
Park shinyeong <syngh503@gmail.com>
2022-06-17 23:40:11 +0900

    fix: 채용 공고 수정 기능 수정
    채용 공고 수정 요청 시 Body에 모든 Column을 넣지 않아도 되도록 수정했습니다.
    update_recruitments.dto.ts 에 IsOptional()을 추가했습니다.
    
    Resolve: #14
    Ref: #14
    Relate to: #4, #14

--- src/recruitments/dto/update_recruitments.dto.ts
+++ src/recruitments/dto/update_recruitments.dto.ts
@@ -1,15 +1,19 @@
-import { IsString, IsNumber } from 'class-validator';
+import { IsString, IsNumber, IsOptional } from 'class-validator';
 
 export class UpdateRecruitmentDTO {
     @IsString()
+    @IsOptional()
     readonly recruit_position: string; 
 
     @IsNumber()
+    @IsOptional()
     readonly recruit_compensation: number; 
 
     @IsString()
+    @IsOptional()
     readonly recruit_content: string; 
 
     @IsString()
+    @IsOptional()
     readonly stack: string; 
 }; 
